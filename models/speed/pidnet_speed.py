17915f0905f829003cad0a64bd866ab08523a70e
Darth-Kronos <purushothaman.syadav@gmail.com>
2023-08-24 13:46:44 -0400

    Speed measurements code

--- models/speed/pidnet_speed.py
+++ models/speed/pidnet_speed.py
@@ -1,6 +1,3 @@
-# ------------------------------------------------------------------------------
-# Written by Jiacong Xu (jiacong.xu@tamu.edu)
-# ------------------------------------------------------------------------------
 import argparse
 import torch
 import torch.nn as nn
@@ -8,6 +5,11 @@ import torch.nn.functional as F
 import time
 from model_utils_speed import BasicBlock, Bottleneck, segmenthead, DAPPM, PAPPM, PagFM, Bag, Light_Bag
 import logging
+import onnx
+import onnxruntime as ort
+import numpy as np
+import os
+
 
 BatchNorm2d = nn.BatchNorm2d
 bn_mom = 0.1
@@ -19,6 +21,7 @@ def parse_args():
     parser.add_argument('--a', help='pidnet-s, pidnet-m or pidnet-l', default='pidnet-s', type=str)
     parser.add_argument('--c', help='number of classes', type=int, default=19)
     parser.add_argument('--r', help='input resolution', type=int, nargs='+', default=(1024,2048))     
+    parser.add_argument('--f', help='model format', type=str, default="pytorch")     
 
 
     args = parser.parse_args()
@@ -214,6 +217,7 @@ def get_seg_model(cfg, imgnet_pretrained):
     
     return model
 
+
 def get_pred_model(name, num_classes):
     
     if 's' in name:
@@ -224,15 +228,10 @@ def get_pred_model(name, num_classes):
         model = PIDNet(m=3, n=4, num_classes=num_classes, planes=64, ppm_planes=112, head_planes=256, augment=False)
     
     return model
-
-if __name__ == '__main__':
-    args = parse_args()
-    # Comment batchnorms here and in model_utils before testing speed since the batchnorm could be integrated into conv operation
-    # (do not comment all, just the batchnorm following its corresponding conv layer)
-    device = torch.device('cuda')
+def pytorch_speed():
     model = get_pred_model(name=args.a, num_classes=args.c)
     model.eval()
-    model.to(device)
+    model.to('cuda')
     iterations = None
     
     input = torch.randn(1, 3, args.r[0], args.r[1]).cuda()
@@ -269,6 +268,190 @@ if __name__ == '__main__':
     torch.cuda.empty_cache()
     FPS = 1000 / latency
     print(FPS)
+
+def torchscript_speed():
+    iterations = None
+    model = torch.jit.load('./pretrained_models/cityscapes/PIDNet_S_Cityscapes_test_traced.ts')
+    
+    input = torch.randn(1, 3, args.r[0], args.r[1]).cuda()
+    with torch.no_grad():
+        for _ in range(10):
+            model(input)
+    
+        if iterations is None:
+            elapsed_time = 0
+            iterations = 100
+            while elapsed_time < 1:
+                torch.cuda.synchronize()
+                torch.cuda.synchronize()
+                t_start = time.time()
+                for _ in range(iterations):
+                    model(input)
+                torch.cuda.synchronize()
+                torch.cuda.synchronize()
+                elapsed_time = time.time() - t_start
+                iterations *= 2
+            FPS = iterations / elapsed_time
+            iterations = int(FPS * 6)
+    
+        print('=========Speed Testing=========')
+        torch.cuda.synchronize()
+        torch.cuda.synchronize()
+        t_start = time.time()
+        for _ in range(iterations):
+            model(input)
+        torch.cuda.synchronize()
+        torch.cuda.synchronize()
+        elapsed_time = time.time() - t_start
+        latency = elapsed_time / iterations * 1000
+    torch.cuda.empty_cache()
+    FPS = 1000 / latency
+    print(FPS)
+
+def onnx_speed():
+    iterations = None
+    image_ortvalue = ort.OrtValue.ortvalue_from_numpy(np.zeros((1,3,1024,2048)), 'cuda', 0)
+    # image_ortvalue = ort.OrtValue.ortvalue_from_numpy(np.zeros((1,3,1024,2048)))
+    session = ort.InferenceSession('./pretrained_models/cityscapes/PIDNet_S_Cityscapes_test.onnx', providers=['TensorrtExecutionProvider','CUDAExecutionProvider', 'CPUExecutionProvider'])
+    io_binding = session.io_binding()
+    io_binding.bind_input(name='input', device_type=image_ortvalue.device_name(),
+                          device_id=0, element_type=np.float32,
+                          shape=image_ortvalue.shape(), buffer_ptr=image_ortvalue.data_ptr())
+    io_binding.bind_output('output')
+    
+    input = torch.randn(1, 3, args.r[0], args.r[1]).cuda()
+    with torch.no_grad():
+        for _ in range(10):
+            session.run_with_iobinding(io_binding)
+    
+        if iterations is None:
+            elapsed_time = 0
+            iterations = 100
+            while elapsed_time < 1:
+                torch.cuda.synchronize()
+                torch.cuda.synchronize()
+                t_start = time.time()
+                for _ in range(iterations):
+                    session.run_with_iobinding(io_binding)
+                torch.cuda.synchronize()
+                torch.cuda.synchronize()
+                elapsed_time = time.time() - t_start
+                iterations *= 2
+            FPS = iterations / elapsed_time
+            iterations = int(FPS * 6)
+    
+        print('=========Speed Testing=========')
+        torch.cuda.synchronize()
+        torch.cuda.synchronize()
+        t_start = time.time()
+        for _ in range(iterations):
+            session.run_with_iobinding(io_binding)
+        torch.cuda.synchronize()
+        torch.cuda.synchronize()
+        elapsed_time = time.time() - t_start
+        latency = elapsed_time / iterations * 1000
+    torch.cuda.empty_cache()
+    FPS = 1000 / latency
+    print(FPS)
+
+def trt_speed():
+    import pycuda.driver as cuda
+    import pycuda.autoinit
+    import tensorrt as trt
+    
+    TRT_LOGGER = trt.Logger()
+
+    def load_engine(engine_file_path):
+        assert os.path.exists(engine_file_path)
+        print("Reading engine from file {}".format(engine_file_path))
+        with open(engine_file_path, "rb") as f, trt.Runtime(TRT_LOGGER) as runtime:
+            return runtime.deserialize_cuda_engine(f.read())
+    
+    engine_file = "/pretrained_models/cityscapes/PIDNet_S_Cityscapes_test.engine"
+    img = torch.randn(1, 3, args.r[0], args.r[1])
+    with load_engine(engine_file) as engine:
+        with engine.create_execution_context() as context:
+            # Set input shape based on image dimensions for inference
+            context.set_binding_shape(engine.get_binding_index("input"), (1, 3, 1024, 2048))
+            # Allocate host and device buffers
+            bindings = []
+            count = 0
+            for binding in engine:
+                count += 1
+                binding_idx = engine.get_binding_index(binding)
+                size = trt.volume(context.get_binding_shape(binding_idx))
+                dtype = trt.nptype(engine.get_binding_dtype(binding))
+                if engine.binding_is_input(binding):
+                    dummy_img = torch.randn((1, 3, 1024, 2048))
+                    input_memory = cuda.mem_alloc(dummy_img.numpy().nbytes)
+                    bindings.append(int(input_memory))
+                else:
+                    print("size and dtype: ", size, dtype)
+                    output_buffer = cuda.pagelocked_empty(size, dtype)
+                    output_memory = cuda.mem_alloc(output_buffer.nbytes)
+                    bindings.append(int(output_memory))
+            stream = cuda.Stream()
+            
+            input_buffer = np.ascontiguousarray(img.numpy())
+            cuda.memcpy_htod_async(input_memory, input_buffer, stream)
+            # Warm up
+            for _ in range(10):
+                context.execute_async_v2(bindings=bindings, stream_handle=stream.handle)
+            iterations = None
+            if iterations is None:
+                elapsed_time = 0
+                iterations = 100
+                while elapsed_time < 1:
+                    stream.synchronize()
+                    stream.synchronize()
+
+                    t_start = time.time()
+                    for _ in range(iterations):
+                        context.execute_async_v2(bindings=bindings, stream_handle=stream.handle)
+                    
+                    stream.synchronize()
+                    stream.synchronize()
+
+                    elapsed_time = time.time() - t_start
+                    iterations *= 2
+                FPS = iterations / elapsed_time
+                iterations = int(FPS * 6)
+        
+            print('=========Speed Testing=========')
+            stream.synchronize()
+            stream.synchronize()
+
+            t_start = time.time()
+            for _ in range(iterations):
+                cuda.memcpy_htod_async(input_memory, input_buffer, stream)
+                context.execute_async_v2(bindings=bindings, stream_handle=stream.handle)
+                cuda.memcpy_dtoh_async(output_buffer, output_memory, stream)
+            
+            stream.synchronize()
+            stream.synchronize()
+            
+            elapsed_time = time.time() - t_start
+            latency = elapsed_time / iterations * 1000
+            # cuda.free()
+            FPS = 1000 / latency
+            print(FPS)
+            
+            
+if __name__ == '__main__':
+    args = parse_args()
+    # Comment batchnorms here and in model_utils before testing speed since the batchnorm could be integrated into conv operation
+    # (do not comment all, just the batchnorm following its corresponding conv layer)
+    
+    if args.f =="pytorch":
+        pytorch_speed()
+    elif args.f == "torchscript":
+        torchscript_speed()
+    elif args.f == "onnx":
+        onnx_speed()
+    elif args.f == "tensorrt":
+        trt_speed()
+
+    
     
     
     
